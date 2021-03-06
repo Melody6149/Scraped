5569c482e9e7ef41dcd58ea3442c50f25bf38338
Melody <reid.eastin@gmail.com>
2021-03-05 23:48:15 -0600

    I made a test scene and a script for moving the pictures

--- Assets/Scripts/PictureGrabingScript.cs
+++ Assets/Scripts/PictureGrabingScript.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Pict : MonoBehaviour
+public class PictureGrabingScript : MonoBehaviour
 {
+    private float distance_to_screen;
+    Vector3 move;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+    void OnMouseDrag()
+    {
+        distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        move = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+        transform.position = new Vector3(move.x, move.y,move.z);
     }
 }
