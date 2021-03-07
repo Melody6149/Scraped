535686fd6705062835ac9851b7ec442b24441feb
Melody <reid.eastin@gmail.com>
2021-03-06 20:23:31 -0600

    added script to keep object from being destroyed

---
+++ Assets/Scripts/ScreenShot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShot : MonoBehaviour
+{
+    int screenshot = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ScreenCapture.CaptureScreenshot("ScreenShots/picture" + screenshot + ".png");
+            screenshot++;
+            Debug.Log("saved");
+        }
+    }
+}
